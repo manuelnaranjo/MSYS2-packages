be4d318375dbeda10750cc0a55302d0dffee53bc
Manuel Naranjo <mnaranjo@irobot.com>
2016-09-01 11:29:52 -0300

    updating lyx-installer: using symlinkx, adding proper lyxrc.dist

--- lyx-installer/install.sh
+++ lyx-installer/install.sh
@@ -1,6 +1,23 @@
 lyxdir=/usr/share/lyx-installer
 
+pre_remove() {
+  for file in $(find ${lyxdir}/bin -type f -name \*.exe ); do
+    fname="$(basename ${file})"
+    fname_noext="${fname%.*}"
+    rm -f /usr/bin/${fname} /usr/bin/${fname_noext}
+  done
+  rm -rf ${lyxdir}/Resources/lyx2lyx
+}
+
+pre_upgrade() {
+  pre_remove;
+}
+
 post_install() {
+  for file in $(find "${lyxdir}/bin" -type f -name \*.exe); do
+    fname="$(basename ${file})"
+    MSYS='winsymlinks:lnk' ln -sf ${file} /usr/bin/${fname}
+  done
   python ${lyxdir}/postinstall.py
 }
 
